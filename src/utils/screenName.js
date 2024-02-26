db84264b90aa44b4ac7a970910f2ffe31a469d45
JiovannyVega <erickjiovannygamezvega@hotmail.com>
2024-02-25 18:16:24 -0600

    Centralizando los nombre de las screens

---
+++ src/utils/screenName.js
@@ -0,0 +1,27 @@
+const accountStack = {
+    tab: "AccountTab"
+}
+
+const favoritesStack = {
+    tab: "FavoritesTab"
+}
+
+const rankingStack = {
+    tab: "RankingTab"
+}
+
+const restaurantStack = {
+    tab: "RestaurantTab"
+}
+
+const searchStack = {
+    tab: "SearchTab"
+}
+
+export const screen = {
+    account: accountStack,
+    favorites: favoritesStack,
+    ranking: rankingStack,
+    restaurant: restaurantStack,
+    search: searchStack
+}
