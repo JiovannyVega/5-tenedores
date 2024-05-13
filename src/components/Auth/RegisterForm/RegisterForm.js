0c5f0d0abec9b84d0d4fd1a21d7e840a886b2470
JiovannyVega <erickjiovannygamezvega@hotmail.com>
2024-05-13 00:37:46 -0600

    50 Validando Formularios

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { View } from 'react-native'
 import { Input, Icon, Button } from 'react-native-elements'
 import { useFormik } from "formik"
-import { initialValues } from './RegisterForm.data'
+import { initialValues, validationSchema } from './RegisterForm.data'
 import { styles } from './RegisterForm.styles'
 
 export function RegisterForm() {
 
     const formik = useFormik({
         initialValues: initialValues(),
+        validationSchema: validationSchema(),
+        validateOnChange: false,
         onSubmit: (formValue) => {
             console.log("Formulario enviado")
             console.log(formValue)
@@ -22,6 +24,7 @@ export function RegisterForm() {
                 containerStyle={styles.input}
                 rightIcon={<Icon type='material-community' name="at" iconStyle={styles.icon} />}
                 onChangeText={text => formik.setFieldValue("email", text)}
+                errorMessage={formik.errors.email}
             />
             <Input
                 placeholder='Contraseña'
@@ -30,6 +33,7 @@ export function RegisterForm() {
                 secureTextEntry={true}
                 rightIcon={<Icon type='material-community' name="eye-outline" iconStyle={styles.icon} />}
                 onChangeText={text => formik.setFieldValue("password", text)}
+                errorMessage={formik.errors.password}
             />
             <Input
                 placeholder='Repetir contraseña'
@@ -38,6 +42,7 @@ export function RegisterForm() {
                 secureTextEntry={true}
                 rightIcon={<Icon type='material-community' name="eye-outline" iconStyle={styles.icon} />}
                 onChangeText={text => formik.setFieldValue("repeatPassword", text)}
+                errorMessage={formik.errors.repeatPassword}
             />
             <Button
                 title="Unirse"
